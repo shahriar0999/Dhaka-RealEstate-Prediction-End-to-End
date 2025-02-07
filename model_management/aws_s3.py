c34993871abaa9363bea6176e5641fbb8e4b880f
shahriar0999 <shahriar0999@proton.me>
2025-02-07 17:31:02 +0600

    modify load_model_from_s3 function

--- model_management/aws_s3.py
+++ model_management/aws_s3.py
@@ -9,28 +9,7 @@ s3 = boto3.client('s3')
 
 
 
-def download_model(file_name):
-    # Make sure the local directory exists
-    local_dir = 'ml-models'
-    os.makedirs(local_dir, exist_ok=True)
-
-    # Define the path where the file should be saved
-    local_file_path = os.path.join(local_dir, file_name.split('/')[-1])
-    
-    s3.download_file(bucket_name, file_name, local_file_path)
-
-# def load_model_from_s3(bucket, key):
-#     try:
-#         obj = s3.get_object(Bucket=bucket, Key=key)
-#         model_data = obj['Body'].read()
-#         # model = joblib.load(BytesIO(model_data))
-#         return model_data
-#     except Exception as e:
-#         print(f"An error occurred while loading the model: {e}")
-#         return None
-
-
-def load_model_from_s3(bucket, key):
+def load_model_from_s3(key, bucket=bucket_name):
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
         buffer = BytesIO(response['Body'].read())
